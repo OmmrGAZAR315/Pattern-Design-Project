35c5f4d4eab518080a1d11930f24bfa72204430f
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2024-05-04 20:39:06 +0300

    patch2 .env

--- src/main/java/org/example/dpproject/DB/EnvLoader.java
+++ src/main/java/org/example/dpproject/DB/EnvLoader.java
@@ -2,13 +2,16 @@ package org.example.dpproject.DB;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class EnvLoader {
     public static void loadEnv() {
         String absolutePath = EnvLoader.class.getResource(".").getPath();
+        String decodedPath = URLDecoder.decode(absolutePath, StandardCharsets.UTF_8);
 
-        try (FileInputStream fis = new FileInputStream(absolutePath.substring(0, absolutePath.indexOf("/target/")) + "/.env")) {
+        try (FileInputStream fis = new FileInputStream(decodedPath.substring(0, absolutePath.indexOf("/target/")) + "/.env")) {
             Properties properties = new Properties();
             properties.load(fis);
             for (String key : properties.stringPropertyNames()) {
